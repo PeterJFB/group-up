a4d19db6166641c0cd463b4e1ec7e11051bd409a
jorgeksa <jorgeksa@stud.ntnu.no>
2022-02-08 18:21:41 +0100

    create tests for authentication api

---
+++ backend/auth/tests/test_setup.py
@@ -0,0 +1,18 @@
+from rest_framework.test import APITestCase
+from django.urls import reverse
+
+
+class TestSetUp(APITestCase):
+    def setUp(self) -> None:
+        self.register_url = reverse("register")
+        self.login_url = reverse("login")
+
+        self.user_data = {
+            'username': 'testmann',
+            'email': 'test@gmail.com',
+            'password': 'pass',
+        }
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
