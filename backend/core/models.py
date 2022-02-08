d1be1cc701a786748f246be38982737c6fdbd3d3
Martin Skatvedt <martinskatvedt@gmail.com>
2022-02-08 14:54:06 +0100

    Create base user with usermanager, add user to adminpanel.
    Co-authored-by: Line Hyggen Rosland <linehro@stud.ntnu.no>

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    def create_user(self, email, name, password):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError("User must have an email address")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create a new superuser profile"""
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, default="")
+    is_staff = models.BooleanField(default=False, verbose_name="staff status")
+
+    # avatar = models.ImageField(upload_to='avatars/', default='avatars/default.png')
+    # birthdate = models.DateField(null=True, blank=True)
+    # interestGroups = models.ManyToManyField(InterestGroup, blank=True)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+
+    def __str__(self):
+        return self.email
