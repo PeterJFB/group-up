60777ea7b466a573dbcb0e27f4c5415c8b812a56
PeterJFB <peterj.flbj@gmail.com>
2022-02-12 14:23:01 +0100

    Modify functions to support authentication and token management
    Co-authored-by: Thorbjørn Stephan Haakon Lundin <tslundin@stud.ntnu.no>

--- frontend/src/api/api.tsx
+++ frontend/src/api/api.tsx
@@ -1,87 +1,82 @@
-type TokenObject = {
+const url = 'localhost:8080'; // TODO: move to environment variable
+
+type TokenBody = {
   token: string;
 };
 
-type AuthenticationBody =
-  | {
-      username: string;
-      password: string;
-    }
-  | {
-      token: string;
-    };
-
-type RegisterApi = (
-  username: string,
-  password: string,
-  success: (successObject: TokenObject) => void,
-  fail: (entry: any) => void
-) => void;
-
-type LoginApi = (
-  username: string,
-  password: string,
-  success: (successObject: TokenObject) => void,
-  fail: (entry: string) => void
-) => void;
-
-export const register: RegisterApi = async (
-  username,
-  password,
-  success,
-  fail
-) => {
-  const response = await fetch(`/auth/login`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      username,
-      password,
-    }),
-  });
-
-  const text = await response.text();
-
-  if (response.status === 200) {
-    console.log('success', JSON.parse(text));
-    const tokenObject: TokenObject = JSON.parse(text);
-
-    localStorage.setItem('token', tokenObject.token);
-
-    success(JSON.parse(text));
-  } else {
-    console.log('failed', text);
-    Object.entries(JSON.parse(text)).forEach(([key, value]) => {
-      fail(`${key}: ${value}`);
-    });
-  }
-};
-
-export const login: LoginApi = async (username, password, success, fail) => {
+/**
+ * The default wrapper for any request after authentication
+ *
+ * @param endpoint api endpoint
+ * @param method e.g. POST, PUT, GET etc.
+ * @param body request body
+ */
+export async function fetchWithToken<ResponseBody>(
+  endpoint: string,
+  method: string,
+  body: any
+) {
   const token = localStorage.getItem('token');
 
-  const requestBody: AuthenticationBody = token
-    ? {token}
-    : {username, password};
+  if (!token) {
+    return {missingToken: true};
+  }
 
-  const response = await fetch(`/auth/login`, {
-    method: 'POST',
+  const response = await fetch(url + endpoint, {
+    method: method,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Token ${token}`,
+    },
+    body,
+  });
+
+  const responseBody: ResponseBody = await response.json();
+
+  return {
+    headers: response.headers,
+    status: response.status,
+    body: responseBody,
+    missingToken: false,
+  };
+}
+
+/**
+ * Wrapper for any authentication request. When successful, it saves a token, allowing the use of `fetchWithToken` for later requests
+ *
+ * @param username username for authentication, i.e. the users email
+ * @param password the password for the user attempting to authenticate.
+ */
+export async function signInAndSaveToken(username: string, password: string) {
+  const response = await fetch(url + '/login', {
+    method: 'GET',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(requestBody),
+    body: {
+      username,
+      password,
+    } as any,
   });
+  const responseBody: TokenBody = await response.json();
 
-  const text = await response.text();
+  const token = responseBody.token;
 
-  if (response.status === 200) {
-    success(JSON.parse(text));
-  } else {
-    console.error('failed', text);
-    fail(JSON.parse(text));
+  localStorage.setItem('token', token);
+
+  const validateToken = localStorage.getItem('token');
+  if (token !== validateToken) {
+    return {
+      savedToken: false,
+    };
   }
-};
+
+  return {
+    headers: response.headers,
+    status: response.status,
+    token: token,
+    savedToken: true,
+  };
+}
