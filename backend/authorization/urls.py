1997c2503d1de4685c9c3535f2262f68f94d36ce
PeterJFB <peterj.flbj@gmail.com>
2022-02-17 16:10:57 +0100

    Perform final linting

--- backend/authorization/urls.py
+++ backend/authorization/urls.py
@@ -2,16 +2,16 @@ from django.urls import path
 from rest_framework.authtoken import views as auth_views
 from .views import RegisterView
 
-# The `urlpatterns` variable is a list of all the URLs that can be requested from the server. 
-# 
-# The `path` function is a function that takes two arguments: 
-# 
-# 1. The URL that you want to add to the list of URLs that can be requested from the server. 
-# 
-# 2. The view function that you want to run when the URL is requested. 
-# 
+# The `urlpatterns` variable is a list of all the URLs that can be requested from the server.
+#
+# The `path` function is a function that takes two arguments:
+#
+# 1. The URL that you want to add to the list of URLs that can be requested from the server.
+#
+# 2. The view function that you want to run when the URL is requested.
+#
 # The `auth_views` module is a module that Django provides that contains a number of pre-built view
-# functions. 
+# functions.
 urlpatterns = [
     path("login/", auth_views.obtain_auth_token, name="login"),
     path("register/", RegisterView.as_view(), name="register"),
